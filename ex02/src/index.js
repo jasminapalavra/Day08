ed50db8c6efa27b62d4bfbd35c9b726a4eb786ab
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-16 08:40:02 +0100

    Added ex02 src and index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+
+
+  function myFunction(parameter1, parameter2) {
+      console.log(parameter1, parameter2);
+  }
+  myFunction("Hi","Padawan");
+  
+  function functionPrintInfo() {
+    console.log("John", 23);
+   console.log("Michael", 43);          
+
+}
+    
+functionPrintInfo();
+
+module.exports = functionPrintInfo;
+
