34ac56d997f72ad8488d6b3d9f6ce903221049bf
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-16 10:06:04 +0100

    Added ex12 src and index.js

---
+++ ex12/src/index.js
@@ -0,0 +1,12 @@
+
+ var randomNumberBetween0and19 = Math.floor(Math.random() * 30);
+
+ function randomInteger() {
+ 
+   // Only change code below this line.
+ 
+   return Math.floor(Math.random() * 10);
+ }
+ 
+console.log(randomNumberBetween0and19);
+module.exports = randomInteger;
